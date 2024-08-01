7410216a823ecdca6ba984731c9e5ffb10e5409f
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-08-01 05:49:54 +0000

    feat: storing original path for the files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use std::{
   env, fs,
+  io::Write,
   path::{Path, PathBuf},
   process::exit,
 };
@@ -9,20 +10,65 @@ use std::{
 #[derive(Debug, Parser)]
 #[command(version)]
 struct Args {
-  file: PathBuf,
+  /// File to drash
+  file: Option<PathBuf>,
+
+  /// List files/directories in the drash
+  #[arg(short, long)]
+  list: bool,
+
+  /// Empty drash
+  #[arg(short, long)]
+  empty: bool,
 }
 
 fn main() -> anyhow::Result<()> {
   let drash_dir = Path::new(&env::var("HOME")?).join(".local/share/Drash");
+  let drash_files = Path::new(&drash_dir).join("files");
+  let drash_info_dir = Path::new(&drash_dir).join("info");
+
   if !drash_dir.exists() {
     fs::create_dir(&drash_dir).inspect_err(|err| {
       eprintln!("Failed to create directory at {:?}", drash_dir);
       eprintln!("Error message: {err}");
       exit(1);
     })?;
+
+    fs::create_dir(Path::new(&drash_dir).join("info")).inspect_err(|err| {
+      eprintln!("Failed to create directory at {:?}", drash_info_dir);
+      eprintln!("Error message: {err}");
+      exit(1);
+    })?;
+
+    fs::create_dir(&drash_files).inspect_err(|err| {
+      eprintln!("Failed to create directory at {:?}", drash_files);
+      eprintln!("Error message: {err}");
+      exit(1);
+    })?;
   }
   let args = Args::parse();
-  fs::rename(&args.file, Path::new(&drash_dir).join(&args.file))?;
+  
+  if let Some(file) = args.file.as_ref() {
+    let current_file = env::current_dir()?.join(file);
+    let mut buffer = fs::OpenOptions::new()
+      .write(true)
+      .append(true)
+      .create(true)
+      .open(Path::new(&drash_info_dir).join(format!("{}.drashinfo", file.display())))?;
+
+    buffer.write_all(b"[Drash Info]\n")?;
+    let formatted_string = format!("Path={}\n", current_file.display());
+    buffer.write_all(formatted_string.as_bytes())?;
+    fs::rename(file, Path::new(&drash_files).join(file))?;
+  }
+
+  if args.empty {
+    !unimplemented!("need to work on this")
+  }
+
+  if args.list {
+    unimplemented!("also need to work on this");
+  }
 
   Ok(())
 }

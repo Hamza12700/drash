7df4f5b7f0aea30cf6b3f68b82b6377e9e0e5e13
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-08-13 05:31:26 +0000

    feat(restore): command overwite prompt improved

--- src/utils.rs
+++ src/utils.rs
@@ -1,20 +1,19 @@
-use colored::Colorize;
+use inquire::Confirm;
 use std::{
   fs,
-  io::{self, Write},
   path::PathBuf,
 };
 
 pub fn check_path(path: &PathBuf, drash_files: &PathBuf, drash_info_dir: &PathBuf) -> bool {
   if path.exists() {
-    println!("\n{}: {:?}", "File already exists".bold(), path.file_name().unwrap());
-    print!("Do you want overwrite it? {}: ", "(Y/n)".bold());
-    io::stdout().flush().unwrap();
+    let file_name = path.file_name().unwrap().to_str().unwrap();
+    let ans = Confirm::new(format!("File already exists: {file_name}").as_str())
+      .with_default(false)
+      .with_help_message("Do you want to overwrite it?")
+      .prompt()
+      .unwrap();
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let input = input.trim();
-    if input == "n" {
+    if !ans {
       return true;
     }
 

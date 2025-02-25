6dce113dcd724e4272eb55fc7505f02565f64282
Hamza <72913359+Hamza12700@users.noreply.github.com>
2025-02-25 13:32:11 +0000

    fix: forgot to exit with errno code in error assert

--- src/assert.c
+++ src/assert.c
@@ -8,7 +8,6 @@
 #include <string.h>
 
 // Assert and display the message
-// Exit with -1
 #define assert(bool, msg) { \
   if (bool) { \
     fprintf(stderr, "[ASSERT FAILED] %s:%d\n", __FILE__, __LINE__); \
@@ -24,7 +23,7 @@
     fprintf(stderr, "[ASSERT ERROR] %s:%d\n", __FILE__, __LINE__); \
     fprintf(stderr, "Reason: %s\n", msg); \
     fprintf(stderr, "Error: %s\n", strerror(errno)); \
-    exit(-1); \
+    exit(errno); \
   } \
 }
 

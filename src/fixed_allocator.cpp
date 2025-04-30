31cc757f05463bd12db3760b4c393228e41b1132
Hamza <72913359+Hamza12700@users.noreply.github.com>
2025-04-30 12:53:36 +0000

    remove function to grow the memory buffer for fixed-allocator
    Can't grow the memory buffer for fixed-allocator because the addresses are absolute, everything
    that has reference to those absolute pointers will then be invalid

--- src/fixed_allocator.cpp
+++ src/fixed_allocator.cpp
@@ -10,40 +10,11 @@ struct Fixed_Allocator {
    int size = 0;      // Used memory
    void *buffer;      // Mapped zero-initialized memory
 
-   int  ralloc(const int bytes); // Returns memory address relative to the mapped memory
    void *alloc(const int bytes);
    void reset();
    void free();
-
-private:
-   void realloc(const int bytes); // Private because it changes the (*buffer) pointer to point to newly allocated memory address and unmap's the old memory address
 };
 
-int Fixed_Allocator::ralloc(const int bytes) {
-   if ((size + bytes) > capacity) {
-      int new_size = page_size;
-      while (new_size < size + bytes) new_size *= 2;
-      realloc(new_size);
-   }
-
-   int ret = size;
-   size += bytes;
-   return ret;
-}
-
-void Fixed_Allocator::realloc(const int bytes) {
-   int page_align_size = page_size;
-   while (page_align_size < bytes) page_align_size *= 2;
-
-   buffer = mremap(buffer, capacity, page_align_size, MREMAP_MAYMOVE); // This unmap's the old memory
-   assert_err(buffer == MAP_FAILED, "mremap failed");
-   capacity = page_align_size;
-}
-
-// @NOTE:
-//
-// We can't call 'realloc' in the routine because then the old mapped memory gets unmapped
-// so, every thing that holds a pointer to that pointer mapped memory would no longer be vaild.
 void *Fixed_Allocator::alloc(const int bytes) {
    assert((size + bytes) > capacity, "(fixed-allocator) allocation failed because capacity is full");
    void *memory = {0};
